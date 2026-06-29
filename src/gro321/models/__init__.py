2220bd4666e45cc160edf66a90a19d61695cfe44
fa0xe420 <hdk77731@gmail.com>
2026-06-29 09:03:38 -0400

    little change

--- src/gro321/models/__init__.py
+++ src/gro321/models/__init__.py
@@ -2,7 +2,10 @@
 Module models - Classes représentant les entités du système de gestion.
 """
 
-from .bon_travail import BonTravail, BonDiagnostic, BonMiseAJour, BonReparation
+from .bon_travail import BonTravail
+from .bon_diagnostic import BonDiagnostic
+from .bon_mise_a_jour import BonMiseAJour
+from .bon_reparation import BonReparation
 from .client import Client
 from .robot import Robot
 
